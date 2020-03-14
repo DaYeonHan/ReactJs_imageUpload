48a021f6b08c70642b3524a0f837740726a177b2
DaYeonHan <spaky1637@gmail.com>
2020-03-14 12:39:44 -0400

    console file path when clicked update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState }  from 'react';
+import React, { useState, Component }  from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, 
   Form,
@@ -22,27 +22,65 @@ function UploadModal(props) {
         </Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form id='imageForm'>
+        <ImageUpload/>
+      </Modal.Body>
+    </Modal>
+  );
+}
+
+class ImageUpload extends Component {
+
+  state = {
+      file: '',
+      imagePreviewUrl: ''
+  };
+
+  _handleSubmit = e => {
+      e.preventDefault();
+      // TODO: do something with -> this.state.file
+      console.log(this.state.file)
+  }
+
+  _handleImageChange = e =>{
+      e.preventDefault();
+
+      let reader = new FileReader();
+      let file = e.target.files[0];
+      reader.onloadend = () => {
+          this.setState({ file: file, imagePreviewUrl: reader.result });
+      }
+      reader.readAsDataURL(file)
+  }
+
+  render() {
+      let {imagePreviewUrl} = this.state;
+      let $imagePreview = null;
+
+    return (
+      <div>
+      <form id='imageForm' onSubmit={this._handleSubmit}>
           <div className="uploadContainer">
             <i className="far fa-file-image fa-3x"></i>
             <br />
               <p className="desc"><span className="contentBold">Upload image</span><br />Write down the link to your client to insert an image</p>
             <br />
-            <Form.Control size="sm" type="url" name="urlFill" id="dataUrl" placeholder="http://example.com/" />
+            <Form.Control size="sm" type="url" name="urlFill" id="dataUrl" placeholder="http://example.com/"/>
           </div>
-          <input style={{display: 'none'}} type="file" name="file" id="addCustomLogo" aria-describedby="inputClientUrl"/>
+          <input style={{display: 'none'}} type="file" name="file" id="addCustomLogo" aria-describedby="inputClientUrl" onChange={this._handleImageChange}/>
           <label htmlFor="addCustomLogo" id="labelCustom">
             Add Custom Logo
           </label>
           <br />
-          <Button id="addClient" variant="primary" type="submit">
+          {/* {!$imagePreview && <img src={imagePreviewUrl} />} */}
+          <Button id="addClient" variant="primary" type="submit" onClick={this._handleSubmit}>
             ADD CLIENT
           </Button>
         </form>
-      </Modal.Body>
-    </Modal>
-  );
+      </div>
+    )
+  }
 }
+
 
 function AddContent(){
   const [modalShow, setModalShow] = React.useState(false);
@@ -65,7 +103,7 @@ function AddContent(){
 
 function App() {
   const [key, setKey] = useState('home');
-
+  
   return (
     <React.Fragment>
       <Tabs justify id="controlled-tab-example" activeKey={key} onSelect={k => setKey(k)}>
